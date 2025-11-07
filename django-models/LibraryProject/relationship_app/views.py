7d796b2bdec0660289ed5efa502650ce3f97f8e3
Brian Cheru <cheruiyotbrian2000@gmail.com>
2025-11-07 12:46:36 +0300

    update

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -32,7 +32,7 @@ def is_admin(user):
     return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
 
 def is_librarian(user):
-    return hasattr(user, 'userprofile') and user.userprofile.role == 'Librarian, Admin'
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Librarian'
 
 def is_member(user):
     return hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
