f2375db1563d3287f7325e29e4ef4b28cf4ae76e
Brian Cheru <cheruiyotbrian2000@gmail.com>
2025-11-07 12:30:35 +0300

    update

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -41,7 +41,7 @@ class UserProfile(models.Model):
     ]
 
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='Member')
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='Admin')
 
     def __str__(self):
         return f"{self.user.username} - {self.role}"
