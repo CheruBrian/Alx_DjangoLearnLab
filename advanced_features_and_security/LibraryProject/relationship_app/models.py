8f89090660bc54de044b36026c7c4656cad186c8
Brian Cheru <cheruiyotbrian2000@gmail.com>
2025-11-12 17:09:41 +0300

    update

--- advanced_features_and_security/LibraryProject/relationship_app/models.py
+++ advanced_features_and_security/LibraryProject/relationship_app/models.py
@@ -1,7 +1,8 @@
 from django.db import models
-from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.conf import settings
+
 
 class Author(models.Model):
     name = models.CharField(max_length=100)
@@ -42,13 +43,11 @@ class Librarian(models.Model):
         return self.name
 
 class UserProfile(models.Model):
-    ROLE_CHOICES = [
-        ('Admin', 'Admin'),
-        ('Librarian', 'Librarian'),
-        ('Member', 'Member'),
-    ]
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+     return f"{self.user.username} - {self.role}"
+
+    user = models.OneToOneField(user, on_delete=models.CASCADE)
     role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='Member')
 
     def __str__(self):
