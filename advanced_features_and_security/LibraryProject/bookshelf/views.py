435fa831432906fc8bc47bdd5c6ecf9d65add7f3
Brian Cheru <cheruiyotbrian2000@gmail.com>
2025-11-14 09:38:11 +0300

    update

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import permission_required
 from .models import Book
+from .forms import ExampleForm
 
 # View to list all books — requires "can_view" permission
 @permission_required('bookshelf.can_view', raise_exception=True)
 def book_list(request):
     books = Book.objects.all()
     return render(request, 'book_list.html', {'books': books})
+
+def search_books(request):
+    query = request.GET.get("q", "")
+    books = Book.objects.filter(title__icontains=query)
+    return render(request, "bookshelf/book_list.html", {"books": books})
