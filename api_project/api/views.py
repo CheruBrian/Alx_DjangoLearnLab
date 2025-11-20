486f1be81d0e318faba4aa8fd9807f751e0a761d
Brian Cheru <cheruiyotbrian2000@gmail.com>
2025-11-20 22:59:37 +0300

    Authentication and password

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,6 +1,8 @@
 from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+
 
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
@@ -8,3 +10,8 @@ class BookList(generics.ListAPIView):
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+def get_permissions(self):
+        if self.action in ['update', 'partial_update', 'destroy']:
+            return [IsAdminUser()]
+        return [IsAuthenticated()]
