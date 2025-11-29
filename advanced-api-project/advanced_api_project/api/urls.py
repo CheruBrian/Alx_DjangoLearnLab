e7ef0ce9a6cdb5b3684a3bffcca3842520792328
Brian Cheru <cheruiyotbrian2000@gmail.com>
2025-11-29 09:09:08 +0300

    deleted

--- advanced-api-project/advanced_api_project/api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('books/', views.BookListCreateView.as_view(), name='book-list'),
-    path('books/<int:pk>/', views.BookDetailView.as_view(), name='book-detail'),
-    path('authors/', views.AuthorListCreateView.as_view(), name='author-list'),
-    path('authors/<int:pk>/', views.AuthorDetailView.as_view(), name='author-detail'),
-]
