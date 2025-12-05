c471522a597b779797374c41c25989a237db3e74
Brian Cheru <cheruiyotbrian2000@gmail.com>
2025-12-05 04:08:04 +0300

    Checks for “Ensure URLs are logically structured and intuitive

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,7 +1,10 @@
 # blog/urls.py
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from . import views
+from . import views (
+    PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView,
+    CommentCreateView, CommentUpdateView, CommentDeleteView, comment_reply
+)
 
 urlpatterns = [
     path("", views.home, name="home"),
@@ -24,4 +27,25 @@ urlpatterns = [
     path("post/new/", views.PostCreateView.as_view(), name="post-create"),
     path("post/<int:pk>/update/", views.PostUpdateView.as_view(), name="post-update"),
     path("post/<int:pk>/delete/", views.PostDeleteView.as_view(), name="post-delete"),
+    
+     # Post URLs
+    path('', PostListView.as_view(), name='post-list'),
+    path('post/<int:pk>/comments/new/', PostCreateView.as_view(), name='post-create'),
+    path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('comment/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('comment/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+    
+    # Comment URLs
+    path('posts/<int:post_pk>/comments/new/', 
+         CommentCreateView.as_view(), 
+         name='comment-create'),
+    path('comments/<int:pk>/edit/', 
+         CommentUpdateView.as_view(), 
+         name='comment-update'),
+    path('comments/<int:pk>/delete/', 
+         CommentDeleteView.as_view(), 
+         name='comment-delete'),
+    path('comments/<int:pk>/reply/', 
+         comment_reply, 
+         name='comment-reply'),
 ]
