1d97097a01a448b2455a2bf57acd7b6ef008f280
Brian Cheru <cheruiyotbrian2000@gmail.com>
2025-12-09 18:12:11 +0300

    Checks for “Add a route in posts/urls.py for the feed endpoint, such as /feed/.” task

---
+++ social_media_api/posts/urls.py
@@ -0,0 +1,13 @@
+from rest_framework.routers import DefaultRouter
+from .views import PostViewSet, CommentViewSet
+from django.urls import path
+from .views import FeedView
+
+router = DefaultRouter()
+router.register(r"posts", PostViewSet, basename="posts")
+router.register(r"comments", CommentViewSet, basename="comments")
+
+urlpatterns = router.urls
+urlpatterns = [
+    path('feed', FeedView.as_view(), name='feed'),
+]
