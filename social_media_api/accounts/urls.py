178d57c9ccaf5ff2a42c027b87f46ccea3586df8
Brian Cheru <cheruiyotbrian2000@gmail.com>
2025-12-09 18:11:12 +0300

    Checks for “Set up URL patterns in accounts/urls.py for follow management (e.g., /follow/int:user_id/ and /unfollow/int:user_id/).” task

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path("register/", RegisterAPIView.as_view(), name="register"),
     path("login/", obtain_auth_token, name="api_token_auth"),  # POST username & password -> token
     path("profile/<str:username>/", UserDetailAPIView.as_view(), name="profile"),
+     path('follow/<int:user_id>', FollowToggleAPIView.as_view(), {'action': 'follow'}, name='follow-user'),
+    path('unfollow/<int:user_id>/', FollowToggleAPIView.as_view(), {'action': 'unfollow'}, name='unfollow-user'),
 ]
