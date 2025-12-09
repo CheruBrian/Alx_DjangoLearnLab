8e36901f8d8235fdbdef46aab632820bc359997e
Brian Cheru <cheruiyotbrian2000@gmail.com>
2025-12-09 17:03:56 +0300

    Checks for “Configure URL patterns in accounts/urls.py to include routes for registration (/register), login (/login), and user profile management (/profile).” task

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -4,6 +4,6 @@ from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path("register/", RegisterAPIView.as_view(), name="register"),
-    path("login", obtain_auth_token, name="api_token_auth"),  # POST username & password -> token
+    path("login/", obtain_auth_token, name="api_token_auth"),  # POST username & password -> token
     path("profile/<str:username>/", UserDetailAPIView.as_view(), name="profile"),
 ]
