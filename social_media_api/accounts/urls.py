75b7f1c237826b9272fd0e4a629c41816bd26c73
Brian Cheru <cheruiyotbrian2000@gmail.com>
2025-12-09 17:02:29 +0300

    Checks for “Configure URL patterns in accounts/urls.py to include routes for registration (/register), login (/login), and user profile management (/profile).” task

---
+++ social_media_api/accounts/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import RegisterAPIView, UserDetailAPIView
+from rest_framework.authtoken.views import obtain_auth_token
+
+urlpatterns = [
+    path("register/", RegisterAPIView.as_view(), name="register"),
+    path("login", obtain_auth_token, name="api_token_auth"),  # POST username & password -> token
+    path("profile/<str:username>/", UserDetailAPIView.as_view(), name="profile"),
+]
